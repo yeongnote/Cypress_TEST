01bf244df45656a052bbb14fdcb381856512c4e9
yeongnote <eongh0640@gmail.com>
2025-04-08 11:15:06 +0900

    Cypress로 CI 추가

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,8 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://practicetestautomation.com/practice-test-login/',
   },
+  projectId: "eig5w1"
+
 };
